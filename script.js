84c7cd073397279f5043051db662ed34ec280de0
Kayla Bachler <chq-kaylab@corp.expeditors.com>
2021-07-08 21:32:57 -0700

    added loading screen

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // Global Variables
 let apiQuotes = [];
 
+// Show that quote is loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 // Pull single quote from array at random
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
     // Check if author field is blank, if true replace with 'Unknown'
@@ -24,11 +38,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set quote and hide the loading screen
     quoteText.textContent = quote.text;
+    complete();
 } 
 
 // Fetch quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
 
     try {
@@ -52,4 +69,3 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 getQuotes();
-
